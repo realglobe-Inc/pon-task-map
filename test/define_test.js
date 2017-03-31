a624fbdccb662fa3d0cc0362f7898202ff74d3f3
Taka Okunishi <okunishinish@gmail.com>
2017-03-31 12:33:00 +0900

    sourceMapを生成するpon-taskを実装した

---
+++ test/define_test.js
@@ -0,0 +1,59 @@
+/**
+ * Test case for define.
+ * Runs with mocha.
+ */
+'use strict'
+
+const define = require('../lib/define.js')
+const ponContext = require('pon-context')
+const { ok } = require('assert')
+const asleep = require('asleep')
+const writeout = require('writeout')
+const browser = require('pon-task-browser')
+const co = require('co')
+
+describe('define', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+  }))
+
+  it('Define', () => co(function * () {
+    let ctx = ponContext()
+
+    let srcDir = `${__dirname}/../misc/mocks`
+    let destDir = `${__dirname}/../tmp/testing-compiled`
+    yield browser(srcDir, destDir, {
+      pattern: '*-entrypoint.js',
+      debug: true
+    })(ctx)
+
+    let task = define(destDir, destDir, {})
+    ok(task)
+
+    yield Promise.resolve(task(ctx))
+  }))
+
+  it('Watch', () => co(function * () {
+    let ctx = ponContext({})
+    let srcDir = `${__dirname}/../tmp/testing-watching/src`
+    let destDir = `${__dirname}/../tmp/testing-watching/dest`
+    let src = srcDir + '/foo-entrypoint.js'
+    yield writeout(src, 'module.exports = "hoge"', { mkdirp: true })
+    yield browser(srcDir, destDir, { pattern: '*-entrypoint.js', debug: true })(ctx)
+    yield asleep(100)
+    define(destDir, destDir, { watchDelay: 1 }).watch(ctx)
+    yield writeout(src, 'module.exports = "fuge"', { mkdirp: true })
+    yield browser(srcDir, destDir, { pattern: '*-entrypoint.js', debug: true })(ctx)
+    yield asleep(200)
+    yield writeout(src, 'module.exports = "moge"', { mkdirp: true })
+    yield browser(srcDir, destDir, { pattern: '*-entrypoint.js', debug: true })(ctx)
+    yield asleep(200)
+  }))
+})
+
+/* global describe, before, after, it */
